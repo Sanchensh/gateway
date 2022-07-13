8d00570610f67d63fa0c5f9160c3d9d1a5e7392e
xujianglei <xujianglei@zofund.com>
2022-07-13 10:56:46 +0800

    添加限流依赖

---
+++ gateway-sentinel/src/main/java/io/gateway/GatewaySentinelApplication.java
@@ -0,0 +1,13 @@
+package io.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaySentinelApplication.class, args);
+    }
+
+}
