cfbeaf416247a9f08340478c0e36d3c19378cb39
xujianglei <xujianglei@zofund.com>
2022-07-14 20:33:27 +0800

    添加web模块

---
+++ gateway-web/src/main/java/io/gateway/GatewayWebApplication.java
@@ -0,0 +1,13 @@
+package io.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWebApplication.class, args);
+    }
+
+}
